65c5bb4ca35104183e7854471d2d010ca568e4cc
Abul Basar <abulbasar@gmail.com>
2020-07-20 00:00:12 +0530

    update

--- src/main/java/com/example/df/UnnestDataFrame.java
+++ src/main/java/com/example/df/UnnestDataFrame.java
@@ -4,10 +4,27 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
 public class UnnestDataFrame {
+
+    private Dataset<Row> flatten(Dataset<Row> input){
+        final Tuple2<String, String>[] dtypes = input.dtypes();
+        Dataset<Row> df = input;
+        for (Tuple2<String, String> dtype : dtypes) {
+            final String name = dtype._1;
+            final String type = dtype._2;
+            if(type.startsWith("StructType")){
+                df = df.selectExpr("*", name + ".*");
+            }
+        }
+        return df;
+    }
+
+
     public void start(String ... args) throws Exception{
 
         if (args.length != 1) {
@@ -25,12 +42,18 @@ public class UnnestDataFrame {
         SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
 
         final Dataset<Row> df = spark.read()
-                .option("header", true)
-                .option("inferSchema", true)
-                .csv(inputPath);
+                .json(inputPath);
 
         df.show();
 
+        flatten(df).show(10, false);
+
+        df.printSchema();
+
+
+        System.out.println("Count of records: " + df.count());
+
+        //df.select("userIdentity.arn").as(Encoders.STRING()).show(10, false);
 
         spark.close();
 
